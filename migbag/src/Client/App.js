9261c707b30f2d58e0816e86c8c7c513de471b3a
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-08-26 04:12:35 +0900

    signin add

--- migbag/src/Client/App.js
+++ migbag/src/Client/App.js
@@ -1,8 +1,17 @@
+import { Router, Routes } from "react-router-dom";
+import '../components/Auth/SignIn/index.jsx'
+import SignIn from "../components/Auth/SignIn";
+import SignUp from "../components/Auth/SignUp";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Router path="/signin" element={<SignIn/>}/>
+      <Router path='/signup' element={<SignUp/>}/>
+      <Router/>
+      <Router/>
+      <Router/>
+    </Routes>
   );
 }
 
