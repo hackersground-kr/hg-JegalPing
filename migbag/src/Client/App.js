997261671bc81b058fca98f2977d727a14b037d3
qtaghdi_ <psm09111@naver.com>
2024-08-26 11:23:51 +0900

    "Fix : 파일 이름 수정"

--- migbag/src/Client/App.js
+++ migbag/src/Client/App.js
@@ -1,17 +1,9 @@
-import { Router, Routes } from "react-router-dom";
-import '../components/Auth/SignIn/index.jsx'
-import SignIn from "../components/Auth/SignIn";
-import SignUp from "../components/Auth/SignUp/Id/index.jsx";
+import React from "react";
 
 function App() {
   return (
-    <Routes>
-      <Router path="/signin" element={<SignIn/>}/>
-      <Router path='/signup' element={<SignUp/>}/>
-      <Router/>
-      <Router/>
-      <Router/>
-    </Routes>
+    <>
+    </>
   );
 }
 
